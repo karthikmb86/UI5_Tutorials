4df485984cfbd947b52d13cfec74a3df47e0570b
karthikmb86 <karthikmb86@gmail.com>
2020-09-18 07:22:01 +0000

    added controller2
    Change-Id: I192c711a90041ffce00d05ea312fc1a1c830b812

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,8 @@
+sap.ui.define(["sap/ui/core/mvc/Controller",
+                "sap/m/MessageToast"], function (Controller, MessageToast) {
+	 Controller.extend("kr.ui5_tutorials.controller.App", {
+	 	onPress: function(){
+	 		MessageToast.show("Hello");
+	 	}
+	});
+	});   
