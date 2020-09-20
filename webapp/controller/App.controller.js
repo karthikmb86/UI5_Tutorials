cfafe8292b9956c6b16081f5d78d76cc46aadf47
karthikmb86 <karthikmb86@gmail.com>
2020-09-20 06:28:27 +0000

    commit with destination
    Change-Id: Ia3a58d6a2d617beb524642a7885d05d6a67d5d00

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -2,7 +2,13 @@ sap.ui.define(["sap/ui/core/mvc/Controller",
                 "sap/m/MessageToast"], function (Controller, MessageToast) {
 	 Controller.extend("kr.ui5_tutorials.controller.App", {
 	 	onPress: function(){
-	 		MessageToast.show("Hello");
+	 		// read msg from i18n model
+			var oBundle = this.getView().getModel("i18n").getResourceBundle();
+			var sRecipient = this.getView().getModel("helloPanel").getProperty("/recipient/name");
+			var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+
+			// show message
+			MessageToast.show(sMsg);
 	 	}
 	});
 	});       
